20ca8df4e767bfca4eeb4ae0af4b926ea1b5717b
Paul Traylor <kungfudiscomonkey@gmail.com>
2023-08-31 20:24:04 +0900

    Check frontmatter

--- scripts/sort.py
+++ scripts/sort.py
@@ -1,9 +1,22 @@
 from pathlib import Path
-from frontmatter import Frontmatter
 
-base = Path(__file__).parent.parent / 'content'/ 'ideas'
+import frontmatter
 
-print(base)
-for src in base.glob('**/*.md'):
-    src.rename(src.with_suffix('.markdown'))
+base = Path(__file__).parent.parent / "content" / "ideas"
 
+for src in base.glob("**/*.md"):
+    print("Fixing file extension for", src.relative_to(base))
+    src.rename(src.with_suffix(".markdown"))
+
+for src in base.glob("**/*.markdown"):
+    update = False
+    print("Checking frontmatter for", src.relative_to(base))
+    post = frontmatter.load(src)
+    if "status" not in post:
+        print('Missing status for ',src)
+        post["status"] = "missing"
+        
+    if update:
+        with src.open("w", encoding="utf8") as fp:
+            fp.write(frontmatter.dumps(post))
+        print("Updated", src.relative_to(base))
