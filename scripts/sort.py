7f7bbcf7d20a1250646e764283668637f52d0c70
Paul Traylor <kungfudiscomonkey@gmail.com>
2024-01-18 10:24:37 +0900

    Update sorting
    Instead of using a separate status, just use the regular 'section'

--- scripts/sort.py
+++ scripts/sort.py
@@ -3,8 +3,18 @@ from pathlib import Path
 import frontmatter
 
 content = Path(__file__).parent.parent / "content"
-ideas = content / "ideas"
+ideas = content / "projects"
 retired = content / "retired"
+
+
+def move(post, src):
+    section = src.relative_to(content).parent
+    alias = f"/{section}/{src.stem}"
+    dst = content / post["status"] / src.name
+    print(alias, dst)
+
+    post.metadata.setdefault("aliases", []).append(f"/{section}/{src.stem}")
+
 
 for src in content.glob("**/*.md"):
     print("Fixing file extension for", src.relative_to(content))
@@ -14,17 +24,27 @@ for src in ideas.glob("**/*.markdown"):
     update = False
     print("Checking frontmatter for", src.relative_to(ideas))
     post = frontmatter.load(src)
-    if "status" not in post:
-        post["status"] = "missing"
-        update = True
 
-    if post["status"] == "retired":
-        post.metadata.setdefault("aliases", []).append(f"/ideas/{src.stem}")
-        src = src.rename(retired / src.name)
-        update = True
+    if "status" not in post:
+        post["status"] = "brainstorm"
+    if post["status"] == "missing":
+        post["status"] = "brainstorm"
+
+    # Move
+    section = src.relative_to(content).parent
+    alias = f"/{section}/{src.stem}"
+    dst = content / post["status"] / src.name
+    post.metadata.setdefault("aliases", []).append(f"/{section}/{src.stem}")
+    post.metadata.pop("status", None)
+    if "aliases" in post.metadata:
+        post["aliases"] = list(set(post["aliases"]))
+    update = True
+
+    dst.parent.mkdir(exist_ok=True)
+    src.rename(dst)
 
     if update:
-        with src.open("w", encoding="utf8") as fp:
+        with dst.open("w", encoding="utf8") as fp:
             fp.write(frontmatter.dumps(post, indent=4))
             fp.write("\n")
         print("Updated", src.relative_to(content))
